5668f16d765b205664978de4a414e78b99803cc9
Genewman <Geneewman4real@gmail.com>
2023-11-21 18:47:48 -0800

    square class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""creates class Square with private instance attribute"""
+
+
+class Square:
+    """defines class with init and instantiate private instance attribute"""
+
+    def __init__(self, size):
+        self.__size = size
